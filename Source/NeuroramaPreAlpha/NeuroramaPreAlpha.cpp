49e159e8a566e0b7b66a2e16956505bd4f6a280a
jellocube <jellocube@gmail.com>
2018-05-27 11:51:28 -0400

    Packaging Issues Fixed, Cleanup Complete

--- Source/NeuroramaPreAlpha/NeuroramaPreAlpha.cpp
+++
@@ -1,6 +0,0 @@
-// Creative Commons Attribution-ShareAlike 4.0 International Public License
-
-#include "NeuroramaPreAlpha.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NeuroramaPreAlpha, "NeuroramaPreAlpha" );
--- Source/NeuroramaPreAlpha/NeuroramaPreAlpha.h
+++
@@ -1,6 +0,0 @@
-// Creative Commons Attribution-ShareAlike 4.0 International Public License
-
-#pragma once
-
-#include "CoreMinimal.h"
-
